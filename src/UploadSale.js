dee9c3b4286358e7a332b7252ba96b5effe3f7a3
איתמר <itamar8zaf@gmail.com>
2023-03-20 23:09:34 +0200

    iyi

---
+++ src/UploadSale.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import Cookies from "js-cookie";
+import {switchClasses} from "@mui/material";
+import ErrorMessage from "./ErrorMessage";
+
+
+function UploadSale() {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState("");
+    const [pictureLink, setPictureLink] = useState("");
+    const [minimumPrice, setMinimumPrice] = useState(0);
+    const [postSale, setPostSale] = useState(false);
+    const [errorCode, setErrorCode] = useState(0);
+
+    const token = Cookies.get("token");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:8989/post-product?token=' + token + '&productName=' + name + '&productDescription=' + description + '&startingPrice=' + minimumPrice + '&productImg=' + pictureLink)
+            .then(response => {
+                if (response.data.success) {
+                    setPostSale(true)
+                } else setErrorCode(response.data.errorCode)
+            });
+    };
+
+    return (
+        <div style={{alignItems:"center"}}>
+            {
+                postSale ? (<h1>Your Product Posted Successfully! </h1>) : (
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            Make a new Offer:
+                            <br/>-------------------
+                        </div>
+                        <label>
+                            Name:
+                            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+                        </label> <br/>
+                        <label>
+                            Description:
+                            <textarea value={description} onChange={e => setDescription(e.target.value)}/>
+                        </label><br/>
+                        <label>
+                            Picture Link:
+                            <input type="text" value={pictureLink} onChange={e => setPictureLink(e.target.value)}/>
+                        </label><br/>
+                        <label>
+                            Minimum Price:
+                            <input type="text" value={minimumPrice} onChange={e => setMinimumPrice(e.target.value)}/>
+                        </label><br/>
+                        <button type="submit">Upload Product</button>
+                        <div>
+                            {
+                                errorCode > 0 &&
+                                <ErrorMessage message={errorCode} lineBreak={true}/>
+                            }
+                        </div>
+
+                    </form>
+                )
+
+            }
+        </div>
+
+
+    );
+}
+
+export default UploadSale;
